bcb835f4b2e6ea7f7f09f9627be87abf4c6e6a47
cete3c-sketch <cete.3c@gmail.com>
2025-11-17 10:12:16 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// ✅ Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCWSesJBDvC2oNSL9UObSVwoEAOzJobAWY",
-  authDomain: "chatbot-dc309.firebaseapp.com",
-  projectId: "chatbot-dc309",
-  storageBucket: "chatbot-dc309.appspot.com",
-  messagingSenderId: "999145904069",
-  appId: "1:999145904069:web:5fb7da6835d5a4ded4ddac",
-  measurementId: "G-6Q1SE7GD6D",
+  apiKey: "AIzaSyCHulLQHzEczsF-EtGCy2hsFqQmEpecst4",
+  authDomain: "chatbot-frontend-70f0e.firebaseapp.com",
+  projectId: "chatbot-frontend-70f0e",
+  storageBucket: "chatbot-frontend-70f0e.firebasestorage.app",
+  messagingSenderId: "776390574734",
+  appId: "1:776390574734:web:b1386822913c39536a3fdf",
+  measurementId: "G-GFHS6JNBW5"
 };
 
 // ✅ Initialize Firebase
@@ -25,3 +24,4 @@ export const db = getFirestore(app);
 
 // ✅ Export auth functions directly
 export { signInWithEmailAndPassword, createUserWithEmailAndPassword };
+
